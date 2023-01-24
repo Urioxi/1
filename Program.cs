0901a843767867190f538de09ff4f8b7869e8a90
Urioxi <urioxa@gmail.com>
2023-01-24 19:39:31 +0100

    redo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Quel est votre pseudo ?");
+      string pseudo = Console.Readline();
+      Console.WriteLine("Bonjour {0}", pseudo);
+    }
+  }
+}
